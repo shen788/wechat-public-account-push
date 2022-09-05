71ae8281708eab57eb5fe10d19af5f3e8360f00b
shen788 <112316527+shen788@users.noreply.github.com>
2022-09-05 14:41:20 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ export const config = {
     // 公众号APP_SECRET
     APP_SECRET: "445c1890a92f3fc132ceb3012546a56a",
     // 模板消息id
-    TEMPLATE_ID: "UTZVBzVDH_iXCSlYWp8A4HpCkKHAtiuklJfanpeI7Wc",
+    TEMPLATE_ID: "SxF-dBfLlmz7rKI8SE1AgNPw9ehDXWeGpg7yizhDwA4",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
     USERS: ["oPlSd5_r3SLuVjIrnKqBuhMBTFsA","oPlSd5yAnuq52UmwcZgfXI5MFfuQ"],
      
